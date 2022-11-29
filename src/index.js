7c56822507a5d2bf2327c5596042c51a380db89f
AllaYakovenko <jaak280488@gmail.com>
2022-11-29 20:49:32 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,76 @@
 import './css/styles.css';
+import Notiflix, { Notify } from 'notiflix';
+import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    input: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+};
+
+refs.input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(evt) { 
+    evt.preventDefault()
+    
+    const name = evt.target.value.trim();
+
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+
+    if (!name) { 
+        return; 
+    }
+
+    fetchCountries(name)
+    .then(countries => {
+        if (countries.length > 10) {
+            return Notify.info("Too many matches found. Please enter a more specific name.");
+        }
+        countryList(countries);
+    })
+        .catch(err => Notify.failure("Oops, there is no country with that name"));
+}
+
+function countryList(countries) { 
+    if (countries.length === 1) {
+        oneCountry(countries);
+    } else { 
+        manyCountries(countries);
+    }
+}
+
+
+function oneCountry(country) { 
+    const markup = country
+        .map(({ flags, name, capital, population, languages }) => {
+            return `<img width="100px" height="100px" 
+                    src='${flags.svg}' alt='${name.official} flag' />
+                    <ul class="one-country__list">
+                        <li class="one-country__item one-country__item--name">
+                            <p>Name: ${name.official}</p></li>
+                        <li class="one-country__item">
+                            <p>Capital: ${capital}</p></li>
+                        <li class="one-country__item">
+                            <p>Population: ${population}</p></li>
+                        <li class="one-country__item">
+                            <p>Languages: ${Object.values(languages)}</p></li>
+                    </ul>`;
+        }).join('');
+    refs.countryInfo.innerHTML = markup;
+}
+
+function manyCountries(countries) { 
+    const markup = countries
+        .map(({ flags, name }) => {
+            return `<li class="countries-list__item">
+                        <img class="countries-list__flag" src="${flags.svg}" 
+                        alt="Flag of ${name.official}" width = 50px height = 50px>
+                            <p class="countries-list__name">${name.official}</p>
+                    </li>`;
+        }).join('');
+    refs.countryInfo.innerHTML = markup;
+}
